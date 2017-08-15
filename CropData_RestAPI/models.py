3f317e9e8fb1494019b1278538c45284d93e4b91
Sandeep <sandeep.a14@iiits.in>
2017-08-15 14:31:47 +0530

    bug fixes in models

--- CropData_RestAPI/models.py
+++ CropData_RestAPI/models.py
@@ -4,53 +4,57 @@ from django.contrib.gis.db import models
 
 # Create your models here.
 class HouseHolds(models.Model):
-	Location=models.PointField(null=True,blank=False)
-	Number_of_members=models.IntegerField()
-	Monthly_income=models.IntegerField()
+	Location  =  models.PointField(null = True,blank = False)
+	Number_of_members  =  models.IntegerField()
+	Monthly_income = models.IntegerField()
+
 class Farms(models.Model):
-	Location=models.PolygonField(null=True,blank=False)
-	HouseHold=models.ForeignKey(HouseHolds, on_delete=models.CASCADE, null=True)
-	TotalArea=models.FloatField()
+	Location  = models.PolygonField(null = True,blank = False)
+	HouseHold = models.ForeignKey(HouseHolds, on_delete = models.CASCADE, null = True)
+	TotalArea = models.FloatField()
 
 class Wells(models.Model):
-	Farm=models.ForeignKey(Farms, on_delete=models.CASCADE, null=True)
-	Depth=models.FloatField()
-	Location=models.PointField(null=True,blank=False)
-	Average_yield=models.FloatField()
+	Farm = models.ForeignKey(Farms, on_delete = models.CASCADE, null = True)
+	Depth = models.FloatField()
+	Location = models.PointField(null = True,blank = False)
+	Average_yield = models.FloatField()
 
 class HouseHold_Photos(models.Model):
-	HouseHold=models.ForeignKey(HouseHolds, on_delete=models.CASCADE, null=True)
-	Image=models.ImageField(upload_to='HouseHold_Photos')
+	HouseHold = models.ForeignKey(HouseHolds, on_delete = models.CASCADE, null = True)
+	Image = models.ImageField(upload_to = 'HouseHold_Photos')
 
 class Farm_Photos(models.Model):
-	Farm=models.ForeignKey(Farms, on_delete=models.CASCADE, null=True)
-	Image=models.ImageField(upload_to='Farm_Photos')
+	Farm  = models.ForeignKey(Farms, on_delete = models.CASCADE, null = True)
+	Image = models.ImageField(upload_to = 'Farm_Photos')
 
 class HouseHold_audio(models.Model):
-	HouseHold=models.ForeignKey(HouseHolds, on_delete=models.CASCADE, null=True)
-	Audio=models.ImageField(upload_to='HouseHold_audio')
+	HouseHold = models.ForeignKey(HouseHolds, on_delete = models.CASCADE, null = True)
+	Audio = models.FileField(upload_to = 'HouseHold_audio')
 
 class Farm_audio(models.Model):
-	Farm=models.ForeignKey(Farms, on_delete=models.CASCADE)
-	Audio=models.ImageField(upload_to='Farm_audio')
+	Farm  = models.ForeignKey(Farms, on_delete = models.CASCADE)
+	Audio = models.FileField(upload_to = 'Farm_audio')
 
-class HouseHolds_members(models.Model):
-	HouseHold=models.ForeignKey(HouseHolds, on_delete=models.CASCADE, null=True)
-	Name=models.CharField(max_length=50)
-	Relation=models.CharField(max_length=50)
-	Age=models.IntegerField()
+class HouseHold_members(models.Model):
+	HouseHold = models.ForeignKey(HouseHolds, on_delete = models.CASCADE, null = True)
+	Name = models.CharField(max_length = 50)
+	Relation = models.CharField(max_length = 50)
+	Age = models.IntegerField()
+
 class Well_Yield(models.Model):
-	Farm=models.ForeignKey(Farms, on_delete=models.CASCADE)
-	Yield=models.FloatField()
-	Time=models.DateTimeField(auto_now_add=True, blank=False)
+	Well = models.ForeignKey(Wells, on_delete = models.CASCADE)
+	Yield = models.FloatField()
+	Time = models.DateTimeField(auto_now_add = True, blank = False)
 
 
-class Season(models.Model):
-	Season=models.CharField(max_length=50)
-class Crop(models.Model):
-	Farm=models.ForeignKey(Farms, on_delete=models.CASCADE)
-	Season=models.ForeignKey(Season,on_delete=models.CASCADE)
-	Crop=models.CharField(max_length=50)
+class Seasons(models.Model):
+	Season = models.CharField(max_length = 50)
+
+class Crops(models.Model):
+	Farm = models.ForeignKey(Farms, on_delete = models.CASCADE)
+	Season = models.ForeignKey(Seasons,on_delete = models.CASCADE)
+	Crop = models.CharField(max_length = 50)
+
 class Extent(models.Model):
-	Crop=models.ForeignKey(Crop,on_delete=models.CASCADE)
-	Extent=models.FloatField()
+	Crop = models.ForeignKey(Crops,on_delete = models.CASCADE)
+	Extent = models.FloatField()
