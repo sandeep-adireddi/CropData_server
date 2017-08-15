fd7ace15cec6ebac647c0a527b14b16492086a09
Sandeep <sandeep.a14@iiits.in>
2017-08-15 14:42:22 +0530

    bug fix

--- CropData_RestAPI/models.py
+++ CropData_RestAPI/models.py
@@ -5,19 +5,19 @@ from django.contrib.gis.db import models
 # Create your models here.
 class HouseHolds(models.Model):
 	Location  =  models.PointField(null = True,blank = False)
-	Number_of_members  =  models.IntegerField()
-	Monthly_income = models.IntegerField()
+	Number_of_members  =  models.IntegerField(null = True,blank = False)
+	Monthly_income = models.IntegerField(null = True,blank = False)
 
 class Farms(models.Model):
 	Location  = models.PolygonField(null = True,blank = False)
 	HouseHold = models.ForeignKey(HouseHolds, on_delete = models.CASCADE, null = True)
-	TotalArea = models.FloatField()
+	TotalArea = models.FloatField(null = True,blank = False)
 
 class Wells(models.Model):
 	Farm = models.ForeignKey(Farms, on_delete = models.CASCADE, null = True)
-	Depth = models.FloatField()
+	Depth = models.FloatField(null = True,blank = False)
 	Location = models.PointField(null = True,blank = False)
-	Average_yield = models.FloatField()
+	Average_yield = models.FloatField(null = True,blank = False)
 
 class HouseHold_Photos(models.Model):
 	HouseHold = models.ForeignKey(HouseHolds, on_delete = models.CASCADE, null = True)
@@ -37,24 +37,24 @@ class Farm_audio(models.Model):
 
 class HouseHold_members(models.Model):
 	HouseHold = models.ForeignKey(HouseHolds, on_delete = models.CASCADE, null = True)
-	Name = models.CharField(max_length = 50)
-	Relation = models.CharField(max_length = 50)
-	Age = models.IntegerField()
+	Name = models.CharField(null = True,blank = False,max_length = 50)
+	Relation = models.CharField(null = True,blank = False,max_length = 50)
+	Age = models.IntegerField(null = True,blank = False)
 
 class Well_Yield(models.Model):
 	Well = models.ForeignKey(Wells, on_delete = models.CASCADE)
-	Yield = models.FloatField()
+	Yield = models.FloatField(null = True,blank = False)
 	Time = models.DateTimeField(auto_now_add = True, blank = False)
 
 
 class Seasons(models.Model):
-	Season = models.CharField(max_length = 50)
+	Season = models.CharField(null = True,blank = False,max_length = 50)
 
 class Crops(models.Model):
 	Farm = models.ForeignKey(Farms, on_delete = models.CASCADE)
 	Season = models.ForeignKey(Seasons,on_delete = models.CASCADE)
-	Crop = models.CharField(max_length = 50)
+	Crop = models.CharField(null = True,blank = False,max_length = 50)
 
 class Extent(models.Model):
 	Crop = models.ForeignKey(Crops,on_delete = models.CASCADE)
-	Extent = models.FloatField()
+	Extent = models.FloatField(null = True,blank = False)
